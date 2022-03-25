352f00dd0a586cebaf2ef6aa7d26cc307e6f2772
Nicholas Ulricksen <n.ulricksen@gmail.com>
2022-03-25 14:28:09 -0700

    fix: exercise 7.5 tests

--- ch7/ex5/limitReader_test.go
+++ ch7/ex5/limitReader_test.go
@@ -7,20 +7,18 @@ import (
 )
 
 func TestLimitReader(t *testing.T) {
-	testBytes := []byte("hello world, today it is hot outside")
+	testString := "hello world, today it is hot outside"
 	var limit int64 = 15
 
-	stringReader := strings.NewReader("")
+	stringReader := strings.NewReader(testString)
 	limitReader := LimitReader(stringReader, limit)
 
-	bytesRead, err := limitReader.Read(testBytes)
-	if len(testBytes) > int(limit) && err == nil {
+	bytesRead := make([]byte, 50)
+	nBytesRead, err := limitReader.Read(bytesRead)
+	if (nBytesRead > int(limit)) && err == nil {
 		t.Errorf("Read() should return EOF error when reading past limit\n")
 	}
 
-	fmt.Printf("LOG: Read %d bytes\n", bytesRead)
-
-	if bytesRead > int(limit) {
-		t.Errorf("Read() read %d bytes, limit is %d\n", bytesRead, limit)
-	}
+	fmt.Printf("LOG: Read %d bytes\n", nBytesRead)
+	fmt.Printf("LOG: %s\n", bytesRead)
 }
